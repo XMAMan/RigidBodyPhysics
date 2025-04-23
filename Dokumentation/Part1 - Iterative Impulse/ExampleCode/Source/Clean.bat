b9f4eaf9e0bf94f61ba2f5d7ef76823435adb444
Enrico Pohl <enricopohl87@googlemail.com>
2025-04-23 05:47:32 +0200

    Use XMAMan.GraphicEngine-NuGet instead of Dlls-references for documentation-exampleprojects

--- Dokumentation/Part1 - Iterative Impulse/ExampleCode/Source/Clean.bat
+++
@@ -1,7 +0,0 @@
-cd .vs && rmdir /S /Q . 2>nul & cd ..
-cd Part1/bin && rmdir /S /Q . 2>nul & cd ../..
-cd Part1/obj && rmdir /S /Q . 2>nul & cd ../..
-cd PhysicEngine/bin && rmdir /S /Q . 2>nul & cd ../..
-cd PhysicEngine/obj && rmdir /S /Q . 2>nul & cd ../..
-cd PhysicEngine.UnitTests/bin && rmdir /S /Q . 2>nul & cd ../..
-cd PhysicEngine.UnitTests/obj && rmdir /S /Q . 2>nul & cd ../..
